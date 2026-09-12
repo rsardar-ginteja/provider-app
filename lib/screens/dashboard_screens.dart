b63267c208f531c618ebf45b86e1f78ff7215938
Rakesh Sardar <rsardar@ginteja.com>
2026-09-12 19:21:34 +0530

    feat: Update message for no active jobs to include accepted jobs

--- lib/screens/dashboard_screens.dart
+++ lib/screens/dashboard_screens.dart
@@ -447,7 +447,7 @@ class _DashboardScreenState extends ConsumerState<DashboardScreen> {
             ),
             const SizedBox(height: 8),
             Text(
-              'No Active Jobs in Progress',
+              'No Active Jobs in Progress or Accepted',
               style: GoogleFonts.inter(
                 fontSize: 12.5,
                 fontWeight: FontWeight.bold,
